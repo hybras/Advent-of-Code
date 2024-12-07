aff6d86475f0d7dd442aa0b4b75ac07fc5139a27
hybras <24651269+hybras@users.noreply.github.com>
2024-12-07 18:40:07 -0500

    template

---
+++ aoc-2024-solutions/day0.py
@@ -0,0 +1,29 @@
+import re
+import aocp
+from typing import List, Tuple, Set, Iterator
+from collections import defaultdict
+from dataclasses import dataclass
+from copy import deepcopy
+from functools import cmp_to_key
+
+
+def part1(input) -> int:
+    pass
+
+
+def part2(input) -> int:
+    pass
+
+def main():
+    # Read input
+    input = open("./input/dayN.txt").readlines()
+
+    # part 1
+    print(f"p1: {part1(input)}")
+
+    # part 2
+    print(f"p2: {part2(input)}")
+
+
+if __name__ == "__main__":
+    main()
